35aa1b8414eeffce1909022b7b709db4f44b4e57
Edu Luque <edu@gudog.com>
2023-10-21 12:24:50 +0200

    Fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ interface Props {
 export const metadata: Metadata = {
   title: "Ask a book",
   description:
-    "This is an experiment in using AI to make my book's content more accessible. Ask a question and AI'll answer it in real-time:",
+    "This is an experiment in using AI to make my book\'s content more accessible. Ask a question and AI\'ll answer it in real-time:",
 };
 
 export default function RootLayout({ children }: Props) {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,13 @@ async function Home() {
       <div className="max-w-prose mb-8">
         <h1>Ask My Book</h1>
         <p>
-          This is an experiment in using AI to make book's content more
-          accessible.
+          {
+            "This is an experiment in using AI to make book\'s content more accessible."
+          }
         </p>
         <ul>
-          <li>1. Choose a book</li>
-          <li>2. Ask a question and AI'll answer it in real-time:</li>
+          <li>{"1. Choose a book"}</li>
+          <li>{"2. Ask a question and AI\'ll answer it in real-time:"}</li>
         </ul>
       </div>
 
